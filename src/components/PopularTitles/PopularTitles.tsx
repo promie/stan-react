c68fa89baf4ada5c0d9b7a4be8d550b29b804507
promie <promie.yutasane@gmail.com>
2022-12-27 09:52:31 +1100

    finalising styling for the popular titles component

--- src/components/PopularTitles/PopularTitles.tsx
+++ src/components/PopularTitles/PopularTitles.tsx
@@ -5,13 +5,28 @@ import PlaceHolder from '../../assets/placeholder.png'
 const PopularTitles: FC = () => {
   return (
     <div className="pop-titles">
-      <div className="image-container">
-        <img
-          src={PlaceHolder}
-          alt="popular series"
-          className="placeholder-img"
-        />
-        <div className="image-overlay">MOVIES</div>
+      <div className="series">
+        <div className="image-container">
+          <img
+            src={PlaceHolder}
+            alt="popular series"
+            className="placeholder-img"
+          />
+          <div className="image-overlay">SERIES</div>
+        </div>
+        <p>Popular Series</p>
+      </div>
+
+      <div>
+        <div className="image-container">
+          <img
+            src={PlaceHolder}
+            alt="popular series"
+            className="placeholder-img"
+          />
+          <div className="image-overlay">MOVIES</div>
+        </div>
+        <p>Popular Movies</p>
       </div>
     </div>
   )
