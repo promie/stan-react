c329c9700366071359f22313fd155c2afb2b637d
promie <promie.yutasane@gmail.com>
2022-12-28 14:31:09 +1100

    styling for the list of series

--- src/pages/Series/Series.tsx
+++ src/pages/Series/Series.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useAppDispatch } from '../../store'
 import { getSeries } from '../../features/shows/showsAction'
+import './series.css'
 
 const Series: FC = () => {
   const { series } = useSelector((store: any) => store.shows)
@@ -11,7 +12,20 @@ const Series: FC = () => {
     dispatch(getSeries())
   }, [])
 
-  return <div>{JSON.stringify(series)}</div>
+  return (
+    <div className="series">
+      {series?.map((ser: any, idx: number) => (
+        <div key={idx} className="poster-container">
+          <img
+            src={ser.images['Poster Art'].url}
+            alt="poster"
+            className="poster"
+          />
+          <p className="series-title">{ser.title}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Series
